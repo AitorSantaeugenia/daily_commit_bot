92fd9f17305d6e7b80cc6bba8096b3d3374a7185
AitorSantaeugenia <aitorsantaeugenia@gmail.com>
2022-11-03 18:09:35 +0100

    feat: changed paths

--- bot.sh
+++ bot.sh
@@ -2,8 +2,8 @@
 
 info="feat: $(date)"
 
-echo "$info" >> /home/your-user/Desktop/projects/commit-bot-main/output.txt
-cd /home/your-user/Desktop/projects/commit-bot-main/
+echo "$info" >> /home/your-user/Desktop/projects/daily-commit-bot/output.txt
+cd /home/your-user/Desktop/projects/daily-commit-bot/
 /usr/bin/git add -u
 /usr/bin/git commit -m "$info"
 /usr/bin/git push
